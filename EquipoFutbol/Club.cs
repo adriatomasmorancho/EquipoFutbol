bd0576ae0d1e73a607f196279a56ca8324eee90a
Adria Tomas <adriatomasmorancho@gmail.com>
2024-06-05 13:19:19 +0200

    New class Club

---
+++ EquipoFutbol/Club.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipoFutbol
+{
+        public class Club
+        {
+            List<Equipo> equipos;
+
+            public Club(List<Equipo> equipos)
+            {
+                this.equipos = new List<Equipo>();
+            }
+
+            public List<Equipo> Equipos { get => equipos; set => equipos = value; }
+        }
+ }
+
