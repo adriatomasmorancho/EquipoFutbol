17ef0a6f8b9cd0e85b860eeea7740ccaf3589dbf
Adria Tomas <adriatomasmorancho@gmail.com>
2024-06-05 12:34:35 +0200

    Add class Partido , and add jugarPartido in Menu

---
+++ EquipoFutbol/Partido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipoFutbol
+{
+    public class Partido
+    {
+        Equipo equipo;
+        Equipo equipo2;
+        Tuple<int, int> marcador;
+        public Partido(Equipo equipo, Equipo equipo2, Tuple<int, int> marcador) 
+        { 
+            this.equipo = equipo;
+            this.equipo2 = equipo2;
+            this.marcador = marcador;
+        }
+
+        public Equipo Equipo { get => equipo; set => equipo = value; }
+        public Equipo Equipo2 { get => equipo2; set => equipo2 = value; }
+        public Tuple<int, int> Marcador { get => marcador; set => marcador = value; }
+
+        public override string ToString()
+        {
+            return "Equipo Local: "+ equipo.Nombre + " | Equipo Visitente : "+ equipo2.Nombre + " | Resultado : " + marcador.Item1 + " - " + marcador.Item2;
+        }
+    }
+}
